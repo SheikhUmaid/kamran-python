f09067614449bd3a1452c216794ecfda7b3a5c8b
Sheikh Umaid <sheikh.umaid03@gmail.com>
2023-06-12 22:24:49 +0530

    Hello world

---
+++ Pyautogui/main.py
@@ -0,0 +1,8 @@
+import pyautogui as pg
+import time
+
+time.sleep(3)
+while True:
+    pg.typewrite('I am a coder', interval=0.0)
+    pg.keyDown('u')
+
---
+++ Pyautogui/trycatch.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Create a list of numbers from 0.1 to 10.1
+numbers = np.arange(0.1, 10.1, 0.1)
+
+# Print the list
+print(type(numbers))
