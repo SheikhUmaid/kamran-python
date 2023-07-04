39cd2fe8289e7a3d3da0cd9b2b5dbc0567be4c1e
Sheikh Umaid <sheikh.umaid03@gmail.com>
2023-07-04 15:09:32 +0530

    oops

---
+++ Object Oriented/oops.py
@@ -0,0 +1,15 @@
+class Police:
+    health = 100
+    ammo = 100
+    armour = 100
+
+
+
+Police1 = Police()
+
+
+
+Police2 = Police()
+
+
+Police3 = Police()
---
+++ Object Oriented/test.py
@@ -0,0 +1,22 @@
+class Student:
+    name = ""
+    roll = ""
+    image = ""
+
+    def __init__(self, name, roll, image):
+        self.name = name
+        self.roll = roll
+        self.image = image
+
+        # print("Created")
+
+
+
+
+
+student1= Student("kamran", "10", "image")
+print(student1.name)
+# student2= Student()
+# # student1.name = "kamran"
+# # student1.roll = "10"
+# # student1.image = "ajshdjh"
